cd3b055e0f48c2b5d31e92072e4d6d137ae4b2bc
Rangga <derangga1011@gmail.com>
2018-02-04 10:00:47 +0700

    first commit mymart

---
+++ frontend/models/Item.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+use yii\behaviors\TimestampBehavior;
+use yii\behaviors\BlameableBehavior;
+/**
+ * This is the model class for table "item".
+ *
+ * Try to modified GII Generator
+ * @property int $id
+ * @property string $nama
+ * @property int $price
+ * @property int $category_id
+ * @property int $created_at
+ * @property int $updated_at
+ * @property int $created_by
+ * @property int $updated_by
+ *
+ * @property ItemCategory $category
+ */
+class Item extends \yii\db\ActiveRecord
+{
+    public function behaviors()
+    {
+        return [
+            TimestampBehavior::className(),
+            BlameableBehavior::className(),
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+
+
+    public static function tableName()
+    {
+        return 'item';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['price', 'category_id'], 'required'],
+            [['price', 'category_id', 'created_at', 'updated_at', 'created_by', 'updated_by'], 'integer'],
+            [['nama'], 'string', 'max' => 255],
+            [['category_id'], 'exist', 'skipOnError' => true, 'targetClass' => ItemCategory::className(), 'targetAttribute' => ['category_id' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nama' => 'Nama',
+            'price' => 'Price',
+            'category_id' => 'Category ID',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+            'created_by' => 'Created By',
+            'updated_by' => 'Updated By',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCategory()
+    {
+        return $this->hasOne(ItemCategory::className(), ['id' => 'category_id']);
+    }
+}
